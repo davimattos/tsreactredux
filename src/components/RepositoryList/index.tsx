5cee990b51bf09e704c07efec4702b9c3054673f
davimattos <dmattos340@gmail.com>
2019-05-25 14:21:25 -0300

    implement map and dispatch props in class

--- src/components/RepositoryList/index.tsx
+++ src/components/RepositoryList/index.tsx
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
 
-export default class RepositoryList extends Component {
-  componentDidMount() {}
+import { Repository } from '../../store/ducks/repositories/types';
+import * as RepositoriesActions from '../../store/ducks/repositories/actions';
+import { ApplicationState } from '../../store';
+
+interface StateProps{
+  repositories: Repository[]
+};
+
+interface DispatchProps{
+  loadRequest(): void
+};
+
+type Props = StateProps & DispatchProps;
+
+class RepositoryList extends Component<Props> {
+  componentDidMount() {
+    const { loadRequest } = this.props;
+
+    loadRequest();
+  };
 
   render() {
+    const { repositories } = this.props;
+
     return (
-      <ul />
+      <ul>
+        {repositories.map(repository => repository.id)}
+      </ul>
     );
   }
 }
+
+const mapStateToProps = (state: ApplicationState) => ({
+  repositories: state.repositories.data,
+});
+
+const dispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators(RepositoriesActions, dispatch);
+
+export default connect(mapStateToProps, dispatchToProps)(RepositoryList);
